c7dca938278a83690d13e77b04fc959e309df782
Henry Buckle <henry.buckle@hiscox.com>
2018-06-04 14:42:29 +0100

    ignore fact on non waf nodes

--- lib/facter/barracudawaf_action_policy.rb
+++ lib/facter/barracudawaf_action_policy.rb
@@ -2,7 +2,11 @@ require_relative '../puppet_x/barracuda/waf/objects'
 
 Facter.add('barracudawaf_action_policy') do
   setcode do
-    action_policies = PuppetX::BarracudaWaf::Objects.list('/security-policies/default/action-policies')
-    action_policies.keys
+    if File.exist?(File.expand_path(File.join(Puppet.settings[:confdir], '/barracudawaf.yaml')))
+      action_policies = PuppetX::BarracudaWaf::Objects.list('/security-policies/default/action-policies')
+      action_policies.keys
+    else
+      []
+    end
   end
 end
