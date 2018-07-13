15bb51aa6873f7144fe3d413b8c1f3b4698a2f92
Henry Buckle <h.buckle@outlook.com>
2018-07-13 10:33:39 +0100

    override instances for admin role

--- lib/puppet/provider/barracudawaf_administrator_role/restapi.rb
+++ lib/puppet/provider/barracudawaf_administrator_role/restapi.rb
@@ -7,4 +7,21 @@ Puppet::Type.type(:barracudawaf_administrator_role).provide(
   def self.api_resource
     'administrator-roles'
   end
+
+  def self.instances
+    urls = resource_urls(api_resource_chain[0], api_resource_chain.drop(1))
+    result = urls.flatten.each.collect do |url|
+      objects = PuppetX::BarracudaWaf::Objects.list(url)
+      objects.each.collect do |_name, properties|
+        resource_hash = {}
+        resource_hash[:ensure] = :present
+        resource_hash[:name] = "/#{url}/#{properties['name']}".chomp('/')
+        convert_api_response(properties["Admin Role"]).each do |key, value|
+          resource_hash[key] = value if resource_type.validproperties.include?(key)
+        end
+        new(resource_hash)
+      end
+    end
+    result.flatten
+  end
 end
