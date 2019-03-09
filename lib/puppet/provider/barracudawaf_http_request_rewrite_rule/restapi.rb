68c5aee486a764fbf27f5a2fba0e9687efac8673
EdMW212 <edmw93@outlook.com>
2019-03-09 11:48:38 +0000

    no longer need to fix response

--- lib/puppet/provider/barracudawaf_http_request_rewrite_rule/restapi.rb
+++ lib/puppet/provider/barracudawaf_http_request_rewrite_rule/restapi.rb
@@ -12,20 +12,20 @@ Puppet::Type.type(:barracudawaf_http_request_rewrite_rule).provide(
     ['services']
   end
 
-  def self.instances
-    urls = resource_urls(api_resource_chain[0], api_resource_chain.drop(1))
-    result = urls.flatten.each.collect do |url|
-      objects = PuppetX::BarracudaWaf::Objects.list(url)
-      objects.each.collect do |_name, properties|
-        resource_hash = {}
-        resource_hash[:ensure] = :present
-        resource_hash[:name] = "/#{url}/#{properties['name']}".chomp('/')
-        convert_api_response(properties['HTTP Request Rewrite']).each do |key, value|
-          resource_hash[key] = value if resource_type.validproperties.include?(key)
-        end
-        new(resource_hash)
-      end
-    end
-    result.flatten
-  end
+  # def self.instances
+  #   urls = resource_urls(api_resource_chain[0], api_resource_chain.drop(1))
+  #   result = urls.flatten.each.collect do |url|
+  #     objects = PuppetX::BarracudaWaf::Objects.list(url)
+  #     objects.each.collect do |_name, properties|
+  #       resource_hash = {}
+  #       resource_hash[:ensure] = :present
+  #       resource_hash[:name] = "/#{url}/#{properties['name']}".chomp('/')
+  #       convert_api_response(properties['HTTP Request Rewrite']).each do |key, value|
+  #         resource_hash[key] = value if resource_type.validproperties.include?(key)
+  #       end
+  #       new(resource_hash)
+  #     end
+  #   end
+  #   result.flatten
+  # end
 end
